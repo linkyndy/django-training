f66269cfdc059bc3dce89252196709b51bdfe70d
linkyndy <linkyndy@gmail.com>
2013-09-30 14:19:20 +0300

    Fixed CSRF bug and removed unnecessary code

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
-from polls.models import Poll, Choice
+from polls.models import Poll
 
 def index(request):
 	latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
