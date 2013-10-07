1a171ee98a2c1ce5cd10fab04484829635ff38ed
linkyndy <linkyndy@gmail.com>
2013-10-07 17:28:07 +0300

    Updated models
    Added __unicode__() for models and added a 'related_name' key to foreign key fields.

--- tests/models.py
+++ tests/models.py
@@ -5,23 +5,35 @@ class Test(models.Model):
 	name = models.CharField(max_length=50)
 	description = models.CharField(max_length=200)
 
+	def __unicode__(self):
+		return self.name
+
 
 class Page(models.Model):
-	test = models.ForeignKey(Test)
+	test = models.ForeignKey(Test, related_name='pages')
 
 
 class Question(models.Model):
 	name = models.CharField(max_length=100)
-	page = models.ForeignKey(Page)
+	page = models.ForeignKey(Page, related_name='questions')
+
+	def __unicode__(self):
+		return self.name
 
 
 class Answer(models.Model):
 	name = models.CharField(max_length=100)
 	score = models.IntegerField()
-	question = models.ForeignKey(Question)
+	question = models.ForeignKey(Question, related_name='answers')
+
+	def __unicode__(self):
+		return self.name
 
 
 class Result(models.Model):
 	text = models.CharField(max_length=100)
 	limit = models.IntegerField()
-	test = models.ForeignKey(Test)
+	test = models.ForeignKey(Test, related_name='results')
+
+	def __unicode__(self):
+		return self.text
