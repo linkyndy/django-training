3ce06b31514f8105463cabe439303ab48ea53da9
linkyndy <linkyndy@gmail.com>
2013-10-07 16:44:10 +0300

    Installed tests app to Django project

--- training/settings.py
+++ training/settings.py
@@ -124,7 +124,8 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
-    'polls'
+    'polls',
+    'tests'
 )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
