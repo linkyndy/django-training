dc56cf066b35ed152f0d2abae42e3a06fda2b98b
linkyndy <linkyndy@gmail.com>
2013-10-07 16:43:55 +0300

    Defined models

--- tests/models.py
+++ tests/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Test(models.Model):
+	name = models.CharField(max_length=50)
+	description = models.CharField(max_length=200)
+
+
+class Page(models.Model):
+	test = models.ForeignKey(Test)
+
+
+class Question(models.Model):
+	name = models.CharField(max_length=100)
+	page = models.ForeignKey(Page)
+
+
+class Answer(models.Model):
+	name = models.CharField(max_length=100)
+	score = models.IntegerField()
+	question = models.ForeignKey(Question)
+
+
+class Result(models.Model):
+	text = models.CharField(max_length=100)
+	limit = models.IntegerField()
+	test = models.ForeignKey(Test)
