5cddd1cda6384c3e5ce73d35395d5cbed491f207
linkyndy <linkyndy@gmail.com>
2013-10-08 14:42:50 +0300

    Added views structure
    Added the structure for the views in the tests app.

--- tests/views.py
+++ tests/views.py
@@ -1,1 +1,19 @@
-# Create your views here.
+from django.http import HttpResponse
+
+
+def index(request):
+	"""Base view, showing all tests"""
+
+	return HttpResponse('Tests index')
+
+
+def view(request, test_id):
+	"""Test view, displays a test"""
+
+	return HttpResponse('View test #%s' % test_id)
+
+
+def result(request, test_id):
+	"""Displays the result of a test"""
+
+	return HttpResponse('Result for test #%s' % test_id)
