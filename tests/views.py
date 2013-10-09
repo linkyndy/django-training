bf34bfd3f606440e52bf05b62fb8ed055fa71983
linkyndy <linkyndy@gmail.com>
2013-10-09 17:17:39 +0300

    Added sessions to cope with test page changes
    Current test and page are saved in sessions so that only one test can be taken at the time and pages can not be skipped;
    Added the test index template;
    Added the possibility to give up a test and start another one;

--- tests/views.py
+++ tests/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from models import Test
 
@@ -7,7 +7,10 @@ from models import Test
 def index(request):
 	"""Base view, showing all tests"""
 
-	context = {'tests': Test.objects.all()}
+	context = {
+		'tests': Test.objects.all(),
+		'active_test': request.session.get('test_id', None)
+	}
 
 	return render(request, 'tests/index.html', context)
 
@@ -15,7 +18,25 @@ def index(request):
 def view(request, test_id):
 	"""Test view, displays a test"""
 
-	return HttpResponse('View test #%s' % test_id)
+	if 'test_id' in request.session and \
+	   request.session['test_id'] != test_id:
+		return redirect('tests:view', test_id=request.session['test_id'])
+
+	if 'test_id' not in request.session:
+		request.session['test_id'] = test_id
+
+	page_number = request.session.get('page_number', 0)
+	page = Test.objects.get(pk=test_id).pages.all()[page_number]
+
+	return HttpResponse('View test #%s, page #%s' % (test_id, page.id))
+
+
+def give_up(request):
+	"""Give up current test, stored in the session"""
+
+	request.session.pop('test_id', None)
+	request.session.pop('page_number', None)
+	return redirect('tests:index')
 
 
 def result(request, test_id):
