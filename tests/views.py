6436156438f043d29c851f511805237e0e481426
linkyndy <linkyndy@gmail.com>
2013-10-10 13:17:29 +0300

    Work on test view page
    Added more security on the view;
    Added a template page and context for it.

--- tests/views.py
+++ tests/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 from models import Test
 
@@ -9,26 +9,43 @@ def index(request):
 
 	context = {
 		'tests': Test.objects.all(),
-		'active_test': request.session.get('test_id', None)
+		'active_test': request.session.get('test_id', None),
 	}
 
 	return render(request, 'tests/index.html', context)
 
 
 def view(request, test_id):
-	"""Test view, displays a test"""
+	"""Test view, displays a test's pages"""
 
+	# Attempt to load the test with test_id
+	test = get_object_or_404(Test, pk=test_id)
+
+	# If another test is started, go to that test
 	if 'test_id' in request.session and \
 	   request.session['test_id'] != test_id:
 		return redirect('tests:view', test_id=request.session['test_id'])
 
+	# If test_id is not started, start it
 	if 'test_id' not in request.session:
 		request.session['test_id'] = test_id
 
+	# Get active test's current page number; if test
+	# has been activated now, get default page number
 	page_number = request.session.get('page_number', 0)
-	page = Test.objects.get(pk=test_id).pages.all()[page_number]
 
-	return HttpResponse('View test #%s, page #%s' % (test_id, page.id))
+	page = test.pages.all()[page_number]
+	page_count = test.pages.count()
+
+	# Assign context variables
+	context = {
+		'test': test,
+		'questions': page.questions.all(),
+		'page_number': page_number,
+		'page_count': page_count
+	}
+
+	return render(request, 'tests/view.html', context)
 
 
 def give_up(request):
