9a3dd4cd7d2d7726575ee6609f9cefec95219a92
linkyndy <linkyndy@gmail.com>
2013-10-14 17:11:02 +0300

    Implemented forms in basic tests app functionality
    Added Django forms to simplify form generation and validation;
    Modified the logic a bit and made things more stable.

---
+++ tests/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+
+
+class PageForm(forms.Form):
+	"""Form that handles pages within a test"""
+
+	def __init__(self, *args, **kwargs):
+		page = kwargs.pop('page', None)
+
+		super(PageForm, self).__init__(*args, **kwargs)
+
+		# Fetch page related questions
+		questions = page.questions.all()
+
+		for question in questions:
+			# Returns related answers as list of 2-tuples
+			# e.g.: [(1, 'answer1'), (2, 'answers'), ...]
+			answers = question.answers.values_list('id', 'name')
+
+			# Add radio fields representing each question's answers
+			self.fields['question_{index}'.format(index=question.id)] = \
+				forms.ChoiceField(label=question.name, required=True, 
+								  choices=answers, widget=forms.RadioSelect)
--- tests/views.py
+++ tests/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 
 from models import Test
+from forms import PageForm
 
 
 def index(request):
@@ -18,67 +19,62 @@ def index(request):
 def view(request, test_id):
 	"""Test view, displays a test's pages"""
 
-	# Attempt to load the test with test_id
-	test = get_object_or_404(Test, pk=test_id)
-
 	# If another test is started, go to that test
 	if 'test_id' in request.session and \
 	   request.session['test_id'] != test_id:
-		return redirect('tests:view', test_id=request.session['test_id'])
+		return redirect('tests:view', request.session['test_id'])
+
+	# Attempt to load the test with test_id
+	test = get_object_or_404(Test, pk=test_id)
 
 	# If test_id is not started, start it
 	if 'test_id' not in request.session:
 		request.session['test_id'] = test_id
-		request.session['page_number'] = 0
-		request.session['score'] = 0
+		request.session['page_number'] = 1
+		request.session['page_count'] = test.pages.count()
 
-	page_count = test.pages.count()
 	page_number = request.session['page_number']
-	page = test.pages.all()[page_number]
+	page_count = request.session['page_count']
 
-	question_count = page.questions.count()
+	# This means that a test is finished, so proceed to the results
+	if page_number-1 == page_count:
+		return redirect('tests:result', test_id)
 
-	errors = None
+	page = test.pages.all()[page_number-1]
 
 	# If page form has been submitted
 	if request.method == 'POST':
-		# Check whether data is valid
-		for i in range(0, question_count):
-			if 'q'+str(i) not in request.POST:
-				errors = 'No answer'
+		form = PageForm(request.POST, page=page)
 
-		if errors is None:
-			# Check if there is another page
-			if page_number+1 < page_count:
-				page_number += 1
-				request.session['page_number'] += 1
+		if form.is_valid():
+			request.session['page_number'] += 1
+
+			# Check if there is another page from the test; if not
+			# proceed to the results
+			if page_number < page_count:
+				return redirect('tests:view', test_id)
 			else:
-				request.session.pop('test_id', None)
-				request.session.pop('page_number', None)
-				return redirect('tests:result')
+				return redirect('tests:result', test_id)
 	else:
-		pass
-
-	page = test.pages.all()[page_number]
-	questions = page.questions.all()
+		form = PageForm(page=page)
 
 	# Assign context variables
 	context = {
 		'test': test,
-		'questions': questions,
 		'page_number': page_number,
 		'page_count': page_count,
-		'errors': errors
+		'form': form,
 	}
-
+	
 	return render(request, 'tests/view.html', context)
 
 
 def give_up(request):
 	"""Give up current test, stored in the session"""
 
-	request.session.pop('test_id', None)
-	request.session.pop('page_number', None)
+	del request.session['test_id']
+	del request.session['page_number']
+	del request.session['page_count']
 	return redirect('tests:index')
 
 
