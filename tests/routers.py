7e0ad6ecfad156ce11d7b8992b5687a6bbf94868
linkyndy <linkyndy@gmail.com>
2013-10-24 15:16:33 +0300

    Drafted MySQL master-slave database configuration
    Disabled it until mysql python module is installed.

---
+++ tests/routers.py
@@ -0,0 +1,16 @@
+class MasterSlaveRouter(object):
+	def db_for_read(self, model, **hints):
+		return 'slave'
+
+	def db_for_write(self, model, **hints):
+		return 'master'
+
+	def allow_relation(self, obj1, obj2, **hints):
+		dbs = ['master', 'slave']
+		if obj1.state.db in dbs and \
+		   obj2.state.db in dbs:
+			return True
+		return None
+
+	def allow_syncdb(self, db, model):
+		return True
--- training/settings.py
+++ training/settings.py
@@ -18,8 +18,22 @@ DATABASES = {
         'PASSWORD': '',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
-    }
+    },
+    #'master': {
+    #    'ENGINE': 'django.db.backends.mysql',
+    #    'NAME': 'training',
+    #    'USER': 'root',
+    #    'PASSWORD': '',
+    #},
+    #'slave': {
+    #    'ENGINE': 'django.db.backends.mysql',
+    #    'NAME': 'training_slave',
+    #    'USER': 'root',
+    #    'PASSWORD': '',
+    #}
 }
+
+#DATABASE_ROUTERS = ['tests.routers.MasterSlaveRouter']
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
